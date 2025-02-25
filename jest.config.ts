f381fecc15e7bc8ff1938ba23fb970e46856a5f8
joejansen <josephjansen@gmail.com>
2025-02-24 19:36:43 -0500

    revert unneeded jest config changes

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,6 @@ export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-  moduleNameMapper: {
-    "^@mui/(.*)$": "<rootDir>/node_modules/@mui/$1",
-    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
-  },
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
