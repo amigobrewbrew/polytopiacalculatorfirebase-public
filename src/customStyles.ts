feafed93c58f25130212da4c3fe9fc0ca9c94e80
amigobrewbrew <57629953+amigobrewbrew@users.noreply.github.com>
2025-02-26 15:24:26 +0100

    refactor: update SINGLE_COL_MAX_WIDTH_PX and MAX_WIDTH_PX constants for improved layout on s23u

--- src/customStyles.ts
+++ src/customStyles.ts
@@ -3,9 +3,9 @@ export const MARGIN_BOTTOM_DEFAULT = 1;
 export const CARD_COLOR = "white";
 export const PADDING_DEFAULT = "2%";
 export const BOX_SHADOW_DEFAULT = 5;
-export const SINGLE_COL_MAX_WIDTH_PX = 375;
+export const SINGLE_COL_MAX_WIDTH_PX = 425;
 export const ONE_HUNDRED_PERCENT = "100%";
-export const MAX_WIDTH_PX = 800;
+export const MAX_WIDTH_PX = 900;
 export const SINGLE_COLUMN_WIDTH_PERCENTAGE = 48;
 export const getSecondaryButtonStyles = () => ({
   minWidth: "55px",
