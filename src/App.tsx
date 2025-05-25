e04e07df4261c6cf37d9de3d4c3ab3a50577bb87
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-05-25 20:32:49 +1200

    refactor: wrap providers into app provider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navbar";
 import HelpPage from "./components/helpPage";
 import SecretGame from "./components/secretGame";
 import BattleGroundDetails from "./components/battleGroundDetails";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import envConfig from "./envConfig";
-import { SnackBarProvider } from "./providers/snackbarContextProvider";
-
-// Create theme once outside of component to avoid recreation on every render
-const theme = createTheme({
-    typography: {
-        fontFamily: "Josefin Sans, Arial, sans-serif",
-    },
-});
+import { AppProvider } from "./providers/appProvider";
 
 const App = () => {
     return (
-        <ThemeProvider theme={theme}>
-            <SnackBarProvider>
-                <meta
-                    name="google-site-verification"
-                    content={envConfig.siteVerification}
-                />
-                <React.Fragment key="adsense-script">
-                    <script async src={envConfig.adsenseClient}></script>
-                </React.Fragment>{" "}
-                <NavBar />
-                <div className="containers">
-                    <Routes>
-                        <Route path="/help" element={<HelpPage />} />
-                        <Route path="/" element={<BattleGroundDetails />} />
-                        <Route path="/secretgame" element={<SecretGame />} />
-                        <Route path="*" element={<Navigate to="/" replace />} />
-                    </Routes>
-                </div>
-            </SnackBarProvider>
-        </ThemeProvider>
+        <AppProvider>
+            <meta
+                name="google-site-verification"
+                content={envConfig.siteVerification}
+            />
+            <React.Fragment key="adsense-script">
+                <script async src={envConfig.adsenseClient}></script>
+            </React.Fragment>
+            <NavBar />
+            <div className="containers">
+                <Routes>
+                    <Route path="/help" element={<HelpPage />} />
+                    <Route path="/" element={<BattleGroundDetails />} />
+                    <Route path="/secretgame" element={<SecretGame />} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </div>
+        </AppProvider>
     );
 };
 
