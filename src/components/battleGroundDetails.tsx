5be2d5f28dfc2148a7c719c80882065c1a840dab
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2025-05-25 17:26:34 +1200

    refactor: migrate game version select bar to own component

--- src/components/battleGroundDetails.tsx
+++ src/components/battleGroundDetails.tsx
@@ -33,6 +33,7 @@ import {
     calculateDefenseResult,
     calculateTotalDamage,
 } from "../utils/damageFormulae";
+import GameVersionSelect from "./gameVersionSelect";
 
 const analyticsLogEvent = isLocal ? analytics.logEvent : logEvent;
 
@@ -761,41 +762,13 @@ const BattleGroundDetails = () => {
             </Box>
 
             {versionConfig && (
-                <CardWithShadow sx={{ p: "3px 2%", width: "100%" }}>
-                    <Box
-                        component="span"
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            typography: "body2",
-                        }}
-                    >
-                        <FormControl sx={{ my: 1, minWidth: 120 }} size="small">
-                            <InputLabel id="version-select-label">
-                                Game version
-                            </InputLabel>
-                            <Select
-                                labelId="version-select-label"
-                                id="version-select"
-                                value={versionConfig.version}
-                                label="Game version"
-                                onChange={handleGameVersionChange}
-                            >
-                                {" "}
-                                {Object.entries(versionConfigs)
-                                    .sort(([a], [b]) => b.localeCompare(a))
-                                    .map(([version, config]) => (
-                                        <MenuItem key={version} value={version}>
-                                            {version} - {config.title}
-                                        </MenuItem>
-                                    ))}
-                            </Select>
-                        </FormControl>
-                        <span>
-                            Build version: {versionConfig?.buildVersion}
-                        </span>
-                    </Box>
-                </CardWithShadow>
+                <GameVersionSelect
+                    versionConfig={versionConfig}
+                    versionConfigs={versionConfigs}
+                    handleGameVersionChange={(event) =>
+                        handleGameVersionChange(event)
+                    }
+                />
             )}
         </Box>
     );
